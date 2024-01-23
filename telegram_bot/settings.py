f3cde8e3b36fa257fc70d5e62c0a0d241e835666
Amirjoin <amirjonkarimoff@gmail.com>
2024-01-23 22:36:45 +0500

    tugatilsinendi

--- telegram_bot/settings.py
+++ telegram_bot/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = 'django-insecure-ezw!&+@jwc#7x)zj1jfzc%$y6_gv6wz8cugxu-u7gx2tu*qvnr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
